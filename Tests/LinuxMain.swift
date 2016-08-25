1408d9e6c565abec399dd14425d0134114b9fe9a
Tanner Nelson <me@tanner.xyz>
2016-08-24 23:00:49 -0400

    fix linux main

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,15 +1,26 @@
 import XCTest
+@testable import HMACTests
 @testable import MD5Tests
+@testable import RandomTests
 @testable import SHA1Tests
-@testable import PBKDF2Tests
+@testable import SHA2Tests
 
 XCTMain([
+	// HMAC
+	testCase(HMACTests.allTests),
+
     // MD5
     testCase(MD5Tests.allTests),
+
+    // Random
+    testCase(RandomTests.allTests),
 
     // SHA1
     testCase(SHA1Tests.allTests),
     
-    // PBKDF2
-    testCase(PBKDF2Tests.allTests),
+    // SHA2
+    testCase(SHA224Tests.allTests),
+    testCase(SHA256Tests.allTests),
+    testCase(SHA384Tests.allTests),
+    testCase(SHA512Tests.allTests),
 ])
