229f5d00cca6e95023cdc480834fb255ee3eff81
C0deH4cker <c0deh4cker@gmail.com>
2017-02-24 03:15:37 -0500

    fix

--- Sources/Random/URandom.swift
+++ Sources/Random/URandom.swift
@@ -6,28 +6,39 @@ import libc
     /dev/urandom is a cryptographically secure random generator provided by the OS.
 */
 public final class URandom: Random {
+    public enum Error: Swift.Error {
+        case open(errno_t)
+        case read(errno_t)
+    }
+
     private let file = fopen("/dev/urandom", "rb")
-    
+
     /// Initialize URandom
     public init() {}
-    
+
     deinit {
         fclose(file)
     }
-    
-    private func read(numBytes: Int) -> [Int8] {
+
+    private func read(numBytes: Int) throws -> [Int8] {
+        // The Random protocol doesn't allow init to fail, so we have to
+        // check whether /dev/urandom was successfully opened here
+        guard file != nil else {
+            throw Error.open(errno)
+        }
+
         // Initialize an empty array with space for numBytes bytes
         var bytes = [Int8](repeating: 0, count: numBytes)
-		guard fread(&bytes, 1, numBytes, file) == numBytes else {
-			// If the requested number of random bytes couldn't be read,
-			// we need to fail fast and hard.
-			abort()
-		}
+        guard fread(&bytes, 1, numBytes, file) == numBytes else {
+            // If the requested number of random bytes couldn't be read,
+            // we need to throw an error
+            throw Error.read(errno)
+        }
         return bytes
     }
-    
-    /// Get a byte array of random UInt8s
-    public func bytes(_ num: Int) -> Bytes {
-        return read(numBytes: num).map({ Byte(bitPattern: $0) })
+
+    /// Get a random array of Bytes
+    public func bytes(count: Int) throws -> Bytes {
+        return try read(numBytes: count).map({ Byte(bitPattern: $0) })
     }
 }
