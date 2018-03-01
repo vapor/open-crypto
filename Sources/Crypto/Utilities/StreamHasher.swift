5696acfb6bc90141e4e51e00e51be91cf2fa3cc4
Tanner Nelson <me@tanner.xyz>
2018-03-01 16:08:40 -0500

    nio

--- Sources/Crypto/Utilities/StreamHasher.swift
+++ Sources/Crypto/Utilities/StreamHasher.swift
@@ -1,39 +1,40 @@
-import Foundation
-import Bits
-import Async
+//import Foundation
+//import Bits
+//import Async
+//
+///// Hashes the contents of a byte stream
+/////
+///// When done hashing the stream, call `complete` to receive the hash and reset the hash to it's original state
+/////
+///// [Learn More →](https://docs.vapor.codes/3.0/crypto/hash/#streaming-hashes-async)
+//public final class ByteStreamHasher<Hash>: Async.InputStream where Hash: Crypto.Hash {
+//    /// ByteStreamHasher accepts byte streams
+//    public typealias Input = ByteBuffer
+//
+//    /// The hash context
+//    private let context = Hash()
+//
+//    /// Any errors that have arisen while hashing
+//    private var error: Error?
+//    
+//    /// Creates a new ByteStreamHasher that can hash a stream of bytes
+//    public init() {}
+//
+//    /// Completes the hash and returns the result
+//    public func complete() -> Data {
+//        defer { close() }
+//        context.finalize()
+//        return context.hash
+//    }
+//
+//    public func input(_ event: InputEvent<ByteBuffer>) {
+//        switch event {
+//        case .close: context.reset()
+//        case .error(let error): self.error = error
+//        case .next(let input, let ready):
+//            context.update(input)
+//            ready.complete()
+//        }
+//    }
+//}
 
-/// Hashes the contents of a byte stream
-///
-/// When done hashing the stream, call `complete` to receive the hash and reset the hash to it's original state
-///
-/// [Learn More →](https://docs.vapor.codes/3.0/crypto/hash/#streaming-hashes-async)
-public final class ByteStreamHasher<Hash>: Async.InputStream where Hash: Crypto.Hash {
-    /// ByteStreamHasher accepts byte streams
-    public typealias Input = ByteBuffer
-
-    /// The hash context
-    private let context = Hash()
-
-    /// Any errors that have arisen while hashing
-    private var error: Error?
-    
-    /// Creates a new ByteStreamHasher that can hash a stream of bytes
-    public init() {}
-
-    /// Completes the hash and returns the result
-    public func complete() -> Data {
-        defer { close() }
-        context.finalize()
-        return context.hash
-    }
-
-    public func input(_ event: InputEvent<ByteBuffer>) {
-        switch event {
-        case .close: context.reset()
-        case .error(let error): self.error = error
-        case .next(let input, let ready):
-            context.update(input)
-            ready.complete()
-        }
-    }
-}
