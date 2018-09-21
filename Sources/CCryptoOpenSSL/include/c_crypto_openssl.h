d3654f2875c886a3a86e4ebf65e2e9541d92447e
tanner0101 <me@tanner.xyz>
2018-09-21 18:17:17 +0200

    add missing openssl 1.0 methods to header

--- Sources/CCryptoOpenSSL/include/c_crypto_openssl.h
+++ Sources/CCryptoOpenSSL/include/c_crypto_openssl.h
@@ -14,4 +14,31 @@
 #include <openssl/pkcs12.h>
 #include <openssl/x509v3.h>
 
+#if (OPENSSL_VERSION_NUMBER < 0x10100000L) || defined(LIBRESSL_VERSION_NUMBER)
+EVP_MD_CTX *EVP_MD_CTX_new() {
+    return EVP_MD_CTX_create();
+}
+
+int EVP_MD_CTX_free(EVP_MD_CTX *ctx) {
+    return EVP_MD_CTX_cleanup(ctx);
+}
+
+void RSA_set0_key(RSA *rsa, BIGNUM *n, BIGNUM *e, BIGNUM *d) {
+    rsa->n = n;
+    rsa->e = e;
+    rsa-> d = d;
+}
+
+HMAC_CTX *HMAC_CTX_new() {
+    HMAC_CTX *ptr = malloc(sizeof(HMAC_CTX));
+    HMAC_CTX_init(ptr);
+    return ptr;
+}
+
+void HMAC_CTX_free(HMAC_CTX *ptr) {
+    HMAC_CTX_cleanup(ptr);
+    free(ptr);
+}
+#endif
+
 #endif
