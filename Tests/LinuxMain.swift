e6a1b943965963d8bd3cfe31d3707391604217e9
Tanner Nelson <me@tanner.xyz>
2019-02-12 16:04:27 -0500

    cryptokit 4

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,9 +1,7 @@
 import XCTest
-@testable import CryptoTests
-@testable import RandomTests
+@testable import CryptoKitTests
 
 XCTMain([
-    /// Crypto
     testCase(BCryptTests.allTests),
     testCase(CipherTests.allTests),
     testCase(MD5Tests.allTests),
@@ -11,7 +9,5 @@ XCTMain([
     testCase(SHA1Tests.allTests),
     testCase(SHA2Tests.allTests),
     testCase(OTPTests.allTests),
-
-    /// Random
     testCase(RandomTests.allTests),
 ])
