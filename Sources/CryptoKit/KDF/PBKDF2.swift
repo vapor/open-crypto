227c42d567dc974cbec802d133670468620bc9b6
Tanner Nelson <me@tanner.xyz>
2019-02-21 21:45:38 -0500

    update PBKDF2 w/ cryptodata

--- Sources/CryptoKit/KDF/PBKDF2.swift
+++ Sources/CryptoKit/KDF/PBKDF2.swift
@@ -1,26 +1,4 @@
-import Foundation
-import CNIOOpenSSL
-import Core
-
-/// The requested amount of output bytes from the key derivation
-///
-/// In circumstances with low iterations the amount of output bytes may not be met.
-///
-/// `digest.digestSize * iterations` is the amount of bytes stored in PBKDF2's buffer.
-/// Any data added beyond this limit
-public enum PBKDF2KeySize {
-    case digestSize
-    case fixed(Int)
-    
-    fileprivate func size(for digest: Digest) -> Int {
-        switch self {
-        case .digestSize:
-            return numericCast(digest.algorithm.digestSize)
-        case .fixed(let size):
-            return size
-        }
-    }
-}
+import CCryptoOpenSSL
 
 /// PBKDF2 derives a fixed or custom length key from a password and salt.
 ///
@@ -38,98 +16,101 @@ public enum PBKDF2KeySize {
 ///
 /// https://en.wikipedia.org/wiki/PBKDF2
 public final class PBKDF2 {
-    private let digest: Digest
+    /// The requested amount of output bytes from the key derivation
+    ///
+    /// In circumstances with low iterations the amount of output bytes may not be met.
+    ///
+    /// `digest.digestSize * iterations` is the amount of bytes stored in PBKDF2's buffer.
+    /// Any data added beyond this limit
+    public enum KeySize {
+        case digestSize
+        case fixed(Int)
+        
+        fileprivate func size(for digest: Digest) -> Int {
+            switch self {
+            case .digestSize:
+                return numericCast(digest.algorithm.digestSize)
+            case .fixed(let size):
+                return size
+            }
+        }
+    }
     
     /// MD4 digest powered key derivation.
     ///
     /// https://en.wikipedia.org/wiki/MD4
-    public static var MD4: PBKDF2 { return .init(digest: Crypto.MD4) }
+    public static var MD4: PBKDF2 { return .init(digest: .md4) }
     
     /// MD5 digest powered key derivation.
     ///
     /// https://en.wikipedia.org/wiki/MD5
-    public static var MD5: PBKDF2 { return .init(digest: Crypto.MD5) }
+    public static var MD5: PBKDF2 { return .init(digest: .md5) }
     
     /// SHA-1 digest powered key derivation.
     ///
     /// https://en.wikipedia.org/wiki/SHA-1
-    public static var SHA1: PBKDF2 { return .init(digest: Crypto.SHA1) }
+    public static var SHA1: PBKDF2 { return .init(digest: .sha1) }
     
     /// SHA-224 (SHA-2) digest powered key derivation.
     ///
     /// https://en.wikipedia.org/wiki/SHA-2
-    public static var SHA224: PBKDF2 { return .init(digest: Crypto.SHA224) }
+    public static var SHA224: PBKDF2 { return .init(digest: .sha224) }
     
     /// SHA-256 (SHA-2) digest powered key derivation.
     ///
     /// https://en.wikipedia.org/wiki/SHA-2
-    public static var SHA256: PBKDF2 { return .init(digest: Crypto.SHA256) }
+    public static var SHA256: PBKDF2 { return .init(digest: .sha256) }
     
     /// SHA-384 (SHA-2) digest powered key derivation.
     ///
     /// https://en.wikipedia.org/wiki/SHA-2
-    public static var SHA384: PBKDF2 { return .init(digest: Crypto.SHA384) }
+    public static var SHA384: PBKDF2 { return .init(digest: .sha384) }
     
     /// SHA-512 (SHA-2) digest powered key derivation.
     ///
     /// https://en.wikipedia.org/wiki/SHA-2
-    public static var SHA512: PBKDF2 { return .init(digest: Crypto.SHA512) }
+    public static var SHA512: PBKDF2 { return .init(digest: .sha512) }
+    
+    private let digest: Digest
     
     /// Creates a new PBKDF2 derivator based on a hashing algorithm
-    public init(digest: Digest) {
-        self.digest = digest
+    public init(digest: Digest.Algorithm) {
+        self.digest = .init(algorithm: digest)
     }
     
     /// Derives a key with up to `keySize` of bytes
     ///
     ///
     public func hash(
-        _ password: String,
-        salt: LosslessDataConvertible,
+        _ password: CryptoData,
+        salt: CryptoData,
         iterations: Int32,
-        keySize: PBKDF2KeySize = .digestSize
-    ) throws -> Data {
+        keySize: KeySize = .digestSize
+    ) throws -> CryptoData {
         let keySize = keySize.size(for: digest)
-        let salt = try salt.convertToData()
         
-        var output = Data(repeating: 0, count: keySize)
+        var output = [UInt8](repeating: 0, count: keySize)
         
-        return try salt.withByteBuffer { saltBuffer in
-            try output.withMutableByteBuffer { outputBuffer in
-                let resultCode = PKCS5_PBKDF2_HMAC(
-                    password, Int32(password.count), // password string and length
-                    saltBuffer.baseAddress, Int32(saltBuffer.count), // salt pointer and length
-                    iterations, // Iteration count
-                    self.digest.algorithm.c, // Algorithm identifier
-                    Int32(keySize), outputBuffer.baseAddress // Output buffer
-                )
-                
-                guard resultCode == 1 else {
-                    throw CryptoError.openssl(identifier: "PKCS5_PBKDF2_HMAC", reason: "Failed to hash password using PBKDF2")
+        let res = salt.withUnsafeBytes { saltBuffer in
+            return password.withUnsafeBytes { passwordBuffer in
+                return output.withUnsafeMutableBytes { outputBuffer in
+                    return PKCS5_PBKDF2_HMAC(
+                        passwordBuffer.baseAddress?.assumingMemoryBound(to: Int8.self),
+                        Int32(password.count), // password string and length
+                        saltBuffer.baseAddress?.assumingMemoryBound(to: UInt8.self),
+                        Int32(saltBuffer.count), // salt pointer and length
+                        iterations, // Iteration count
+                        self.digest.algorithm.c, // Algorithm identifier
+                        Int32(keySize),
+                        outputBuffer.baseAddress?.assumingMemoryBound(to: UInt8.self) // Output buffer
+                    )
                 }
             }
-            
-            return output
+        }        
+        guard res == 1 else {
+            throw CryptoError.openssl(identifier: "PKCS5_PBKDF2_HMAC", reason: "Failed to hash password using PBKDF2")
         }
-    }
-}
-
-/// XORs the lhs bytes with the rhs bytes on the same index
-///
-/// Assumes and asserts lhs and rhs to have an equal count
-fileprivate func ^=(lhs: inout Data, rhs: Data) {
-    // These two must be equal for the PBKDF2 implementation to be correct
-    assert(lhs.count == rhs.count)
-    
-    // Foundation does not guarantee that Data is a top-level blob
-    // It may be a sliced blob with a startIndex of > 0
-    var lhsIndex = lhs.startIndex
-    var rhsIndex = rhs.startIndex
-    
-    for _ in 0..<lhs.count {
-        lhs[lhsIndex] = lhs[lhsIndex] ^ rhs[rhsIndex]
         
-        lhsIndex += 1
-        rhsIndex += 1
+        return .bytes(output)
     }
 }
--- Tests/CryptoKitTests/PBKDF2Tests.swift
+++ Tests/CryptoKitTests/PBKDF2Tests.swift
@@ -1,22 +1,22 @@
 import XCTest
-@testable import Crypto
+import CryptoKit
 
-class PBKDF2Tests: XCTestCase {
-    static let allTests = [
+public class PBKDF2Tests: XCTestCase {
+    public func testSanity() throws {
+        let pbkdf2 = PBKDF2.SHA256
+        
+        for (key, salt, iterations, keySize, expectation) in pbkdf2tests {
+            let result = try pbkdf2.hash(key, salt: salt, iterations: iterations, keySize: keySize)
+            XCTAssertEqual(result.hexEncodedString(), expectation.lowercased())
+        }
+    }
+    public static let allTests = [
         ("testSanity", testSanity),
     ]
     
-    func testSanity() throws {
-        let pbkdf2 = PBKDF2.SHA256
-        
-        for (key, salt, iterations, keySize, expectation) in tests {
-            let result = try pbkdf2.hash(key, salt: salt, iterations: iterations, keySize: keySize)
-            XCTAssertEqual(result.hexEncodedString(), expectation)
-        }
-    }
 }
 
-fileprivate let tests: [(key: String, salt: String, iterations: Int32, keySize: PBKDF2KeySize, expectation: String)] = [
+private let pbkdf2tests: [(key: CryptoData, salt: CryptoData, iterations: Int32, keySize: PBKDF2.KeySize, expectation: String)] = [
     ("password", "salt", 1, .fixed(20), "120fb6cffcf8b32c43e7225256c4f837a86548c9"),
     ("password", "salt", 2, .fixed(20), "ae4d0c95af6b46d32d0adff928f06dd02a303f8e"),
     ("password", "salt", 4_096, .fixed(20), "c5e478d59288c841aa530db6845c4c8d962893a0"),
