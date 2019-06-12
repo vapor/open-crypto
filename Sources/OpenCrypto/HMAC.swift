31307b3329c0b0b783dabd2b685a768401df97d4
Tanner Nelson <me@tanner.xyz>
2019-06-12 17:42:34 -0400

    organize hmac

--- Sources/OpenCrypto/HMAC.swift
+++ Sources/OpenCrypto/HMAC.swift
@@ -107,36 +107,3 @@ public struct HMAC<H> where H : HashFunction {
         HMAC_CTX_free(self.context)
     }
 }
-
-public protocol MessageAuthenticationCode : ContiguousBytes, CustomStringConvertible, Hashable, Sequence where Self.Element == UInt8 {
-    static var byteCount: Int { get }
-}
-
-public struct HashedAuthenticationCode<H> : MessageAuthenticationCode where H : HashFunction {
-    public typealias Element = UInt8
-
-    public static var byteCount: Int {
-        guard let opensslDigest = H.Digest.self as? AnyOpenSSLDigest.Type else {
-            fatalError("\(H.self) is not an OpenSSL hash function")
-        }
-        return opensslDigest.byteCount
-    }
-
-    let bytes: [UInt8]
-
-    init(bytes: [UInt8]) {
-        self.bytes = bytes
-    }
-
-    public var description: String {
-        return self.bytes.hexEncodedString()
-    }
-
-    public func makeIterator() -> Array<UInt8>.Iterator {
-        return self.bytes.makeIterator()
-    }
-
-    public func withUnsafeBytes<R>(_ body: (UnsafeRawBufferPointer) throws -> R) rethrows -> R {
-        return try self.bytes.withUnsafeBytes(body)
-    }
-}
